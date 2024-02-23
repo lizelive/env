2a05ea64d6c32d1be10a8aadac834769b6f5d6d7
lizelive <github@lize.live>
2024-02-22 17:06:50 -0800

    Create run.sh

---
+++ nvidia-pytorch/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# -v "$HOME:$HOME:ro" \
+docker run --name=env --rm -it \
+--ipc=host --net=host \
+--gpus all \
+--ulimit memlock=-1 --ulimit=stack=67108864 \
+-u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro \
+-v "$HOME/.vscode:$HOME/.vscode" \
+-v "$HOME/Documents:$HOME/Documents" \
+-v "$HOME/.cache:$HOME/.cache" \
+$(docker build nvidia-pytorch -q)
