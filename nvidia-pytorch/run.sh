04b775fbb02eb68c6eb16d81a0cd296485c3a389
Lize Live <huggingface@lize.live>
2024-03-05 12:08:05 -0800

    torch 24.02

--- nvidia-pytorch/run.sh
+++ nvidia-pytorch/run.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 # -v "$HOME:$HOME:ro" \
-docker run --name=env --rm -it \
---ipc=host --net=host \
---gpus all \
---ulimit memlock=-1 --ulimit=stack=67108864 \
+
+name=mlws
+
+docker rm -f $name || true
+args="-d"
+# args="${args} -it --rm"
+args="${args} --net=host"
+# args="${args} -p=127.0.0.1:8000:8000"
+
+docker run --name=$name $args \
+--ipc=host \
+--gpus=all --ulimit=memlock=-1 --ulimit=stack=67108864 \
 -u $(id -u):$(id -g) -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro \
+-v "$HOME/fakehome:$HOME" \
 -v "$HOME/.vscode:$HOME/.vscode" \
 -v "$HOME/.vscode-server:$HOME/.vscode-server" \
 -v "$HOME/Documents:$HOME/Documents" \
 -v "$HOME/.cache:$HOME/.cache" \
 -w "$HOME/Documents" \
-$(docker build nvidia-pytorch -q)
+$(docker build nvidia-pytorch --pull -q)
